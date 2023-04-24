a1dfd81755547babfe7fa230d7255bd7da27465a
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-25 00:17:35 +0200

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     token: "token_of_your_app",
 
-    guildId: "your_guild_id",
     clientId: "id_of_your_app",
 
     BotClose: false, // put true if you want only you can do the orders
